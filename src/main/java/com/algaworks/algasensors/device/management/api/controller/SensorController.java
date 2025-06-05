ec8dd2af3cdb74a740d7b6294be051f87f603df2
Giovani Andrade <giovani.andrade@triersistemas.com.br>
2025-06-05 08:13:29 -0300

    Implementando a consulta de sensores

--- src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
@@ -6,13 +6,17 @@ import com.algaworks.algasensors.device.management.common.IdGenerator;
 import com.algaworks.algasensors.device.management.domain.model.Sensor;
 import com.algaworks.algasensors.device.management.domain.model.SensorId;
 import com.algaworks.algasensors.device.management.domain.repository.SensorRepository;
+import io.hypersistence.tsid.TSID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/sensors")
@@ -20,6 +24,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class SensorController {
 
     private final SensorRepository sensorRepository;
+
+    // O @PathVariable é que alimenta o parâmetro sensorId
+    // Ele chega como uma String, mas é convertido para TSID pelo conversor StringToTSIDWebConverter
+    @GetMapping("{sensorId}")
+    public SensorOutput getOne(@PathVariable  TSID sensorId) {
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return convertToModel(sensor);
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -35,6 +48,10 @@ public class SensorController {
                 .build();
 
         sensor = sensorRepository.saveAndFlush(sensor);
+        return convertToModel(sensor);
+    }
+
+    private SensorOutput convertToModel(Sensor sensor) {
         return SensorOutput.builder()
                 .id(sensor.getId().getValue())
                 .name(sensor.getName())
